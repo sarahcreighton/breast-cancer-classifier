4143b9614b951e93f8bdd01eab95fb989edf1ed3
sarahcreighton <sarahcreighton@users.noreply.github.com>
2026-03-03 22:30:14 -0500

    update modelling.py and testing notebook

--- src/modelling.py
+++ src/modelling.py
@@ -10,14 +10,16 @@ TO DO: documentation (comments, docstrings, README)
 Consider replacing StandardScaler() with more generic scaler_cls()
 """
 
+import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 
 
-#--- PIPELINE CONSTRUCTORS ---#
+#--- MODEL CONSTRUCTORS ---#
 def logistic_pipeline(penalty="l2", C=1.0, random_state=42):
     """
     Pipeline for logistic regression with optional L1/L2 regularization
@@ -68,3 +70,43 @@ def svm_pipeline(C=1.0, kernel="rbf", random_state=42):
             random_state = random_state
         ))
     ])
+
+
+#--- HYPERPARAMETER GRIDS ---#
+# def logistic_param_grid():
+#     return {
+#         "model__C": [0.01, 0.1, 1, 10],
+#         "model__penalty": ["l1", "l2"]
+#     }
+
+# def rf_param_grid():
+#     return {
+#         "model__n_estimators": [100, 200, 500],
+#         "model__max_depth": [None, 5, 10]
+#     }
+
+# def svm_param_grid():
+#     return {
+#         "model__C": [0.1, 1, 10],
+#         "model__kernel": ["linear", "rbf"]
+#     }
+
+
+#--- HELPER UTILITIES ---#
+
+# # generic tuning
+# def tune_model(pipeline, param_grid, X_train, y_train, cv=5, scoring="roc_auc"):
+#     grid = GridSearchCV(
+#         pipeline,
+#         param_grid,
+#         cv = cv, 
+#         scoring = scoring
+#         # look into n_jobs=-1
+#     )
+#     grid.fit(X_train, y_train)
+#     return grid
+
+# # coefficient extraction
+# def extract_logistic_coefs(pipeline, feature_names):
+#     model = pipeline.named_steps["model"]
+#     return pd.Series(model.coef_[0], index=feature_names)
