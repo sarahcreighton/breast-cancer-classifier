3ba45c9916772f23b0eac881192f008b6ce507f5
sarahcreighton <sarahcreighton@users.noreply.github.com>
2026-03-03 23:16:14 -0500

    built out functionality for hyperparameter grids, generic tuning, coefficient extraction

--- src/modelling.py
+++ src/modelling.py
@@ -91,22 +91,61 @@ def svm_pipeline(C=1.0, kernel="rbf", random_state=42):
 #         "model__kernel": ["linear", "rbf"]
 #     }
 
+def get_param_grid(model_name: str) -> dict:
+    model_name = model_name.lower()
+
+    if model_name == "logistic":
+        return {
+            "model__C": [0.01, 0.1, 1, 10],
+            "model__penalty": ["l1", "l2"],
+        }
+    elif model_name == "rf":
+        return {
+            "model__n_estimators": [100, 200],
+            "model__max_depth": [None, 5, 10],
+        }
+    elif model_name == "svm":
+        return {
+            "model__C": [0.1, 1, 10],
+            "model__kernel": ["linear", "rbf"],
+        }
+    else:
+        raise ValueError("Unknown model name.")
+
 
 #--- HELPER UTILITIES ---#
 
-# # generic tuning
-# def tune_model(pipeline, param_grid, X_train, y_train, cv=5, scoring="roc_auc"):
-#     grid = GridSearchCV(
-#         pipeline,
-#         param_grid,
-#         cv = cv, 
-#         scoring = scoring
-#         # look into n_jobs=-1
-#     )
-#     grid.fit(X_train, y_train)
-#     return grid
+# generic tuning
+def tune_model(pipeline, param_grid, X_train, y_train, cv=5, scoring="roc_auc"):
+    """
+    Perform grid search and return best estimator.
+    """
+    grid = GridSearchCV(
+        estimator = pipeline,
+        param_grid = param_grid,
+        cv = cv, 
+        scoring = scoring
+        # look into n_jobs=-1
+    )
+    
+    grid.fit(X_train, y_train)
+
+    return grid.best_estimator_, grid.best_params_, grid.best_score_
+
 
 # # coefficient extraction
 # def extract_logistic_coefs(pipeline, feature_names):
 #     model = pipeline.named_steps["model"]
 #     return pd.Series(model.coef_[0], index=feature_names)
+
+def extract_feature_importance(pipeline, feature_names):
+    model = pipeline.named_steps["model"]
+
+    if hasattr(model, "coef_"):
+        return dict(zip(feature_names, model.coef_.flatten()))
+    
+    elif hasattr(model, "feature_importances_"):
+        return dict(zip(feature_names, model.feature_importances_))
+    
+    else:
+        return None 
